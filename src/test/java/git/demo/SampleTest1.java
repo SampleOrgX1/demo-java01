a21e0673a2eec92a66a625c8841f8dd4d0df47c0
AlphaAwana <166313032+AlphaAwana@users.noreply.github.com>
2024-04-12 11:26:46 +0530

    Update SampleTest1.java
    Added new commitment

--- src/test/java/git/demo/SampleTest1.java
+++ src/test/java/git/demo/SampleTest1.java
@@ -32,7 +32,7 @@ public class SampleTest1 {
     System.out.println("Selenium Git Test1########");
     driver.findElement(By.name("q")).sendKeys("selenium grid\n");
     System.out.println("Wait for 3s");
-    System.out.println("Adding for pipe version");
+    System.out.println("=== Adding for pipe version ===");
     Thread.sleep(3000);
     
   }
